2117b3bf60cc695c0e81db642f913ddcfc495ed3
hamzahaly <hzhaly93@gmail.com>
2016-11-29 14:59:43 -0800

    added cell files

--- Oishii/Oishii/RecipesTableViewController.swift
+++ Oishii/Oishii/RecipesTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class RecipesTableViewController: UITableViewController {
+    
+    let model = ["Sushi", "Noodles", "Rice Dishes", "Deserts", "Teriyaki"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,23 +31,23 @@ class RecipesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return model.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecipesCell", for: indexPath)
+
+         //Configure the cell...
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
