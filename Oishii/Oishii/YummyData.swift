ab392b0449de015a3351b39e63a1ac541234402d
Thomas <tgents@uw.edu>
2016-12-08 19:05:23 -0800

    commented out save for now

--- Oishii/Oishii/YummyData.swift
+++ Oishii/Oishii/YummyData.swift
@@ -39,7 +39,7 @@ class YummyData: NSObject {
                     self.editors.append(recipe)
                 }
             }
-            self.save()
+            //self.save()
             NSLog("\(self.recipes)")
         })
     }
