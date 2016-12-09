39dcf6020ac6296cfcb0edbb50134d55e2fc6484
Thomas <tgents@gmail.com>
2016-12-09 14:29:06 -0800

    local storage for favorites and editors workingworking

--- Oishii/Oishii/Recipe.swift
+++ Oishii/Oishii/Recipe.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Recipe: NSObject {
+class Recipe: NSObject, NSCoding {
     var recipeid: String = ""
     var name: String = ""
     var shortDescription: String = ""
@@ -17,4 +17,31 @@ class Recipe: NSObject {
     var procedure: [String] = []
     var categories: [String] = []
     var editors: Bool = false
+    
+    override init(){
+        super.init()
+    }
+    
+    required init (coder aDecoder: NSCoder) {
+        super.init()
+        self.recipeid = aDecoder.decodeObject(forKey: "recipeid") as! String
+        self.name = aDecoder.decodeObject(forKey: "name") as! String
+        self.shortDescription = aDecoder.decodeObject(forKey: "short_description") as! String
+        self.longDescription = aDecoder.decodeObject(forKey: "long_description") as! String
+        self.ingredients = aDecoder.decodeObject(forKey: "ingredients") as! [String]
+        self.procedure = aDecoder.decodeObject(forKey: "procedure") as! [String]
+        self.categories = aDecoder.decodeObject(forKey: "categories") as! [String]
+        self.editors = aDecoder.decodeBool(forKey: "editors")
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(recipeid, forKey:"recipeid")
+        aCoder.encode(name, forKey:"name")
+        aCoder.encode(shortDescription, forKey:"short_description")
+        aCoder.encode(longDescription, forKey:"long_description")
+        aCoder.encode(ingredients, forKey:"ingredients")
+        aCoder.encode(procedure, forKey:"procedure")
+        aCoder.encode(categories, forKey:"categories")
+        aCoder.encode(editors, forKey:"editors")
+    }
 }
--- Oishii/Oishii/YummyData.swift
+++ Oishii/Oishii/YummyData.swift
@@ -12,7 +12,8 @@ import FirebaseDatabase
 
 class YummyData: NSObject {
     static let shared = YummyData()
-    private static let filePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "questions"
+    private static let editorsSave = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "editors"
+    private static let favSave = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "favorites"
     var ref: FIRDatabaseReference!
     var recipes: [Recipe] = [Recipe]()
     var favoriteRecipes: [Recipe] = [Recipe]()
@@ -21,9 +22,10 @@ class YummyData: NSObject {
     func setup(){
         FIRApp.configure()
         ref = FIRDatabase.database().reference()
-        //loadOffline()
+        loadOffline()
+        NSLog("\(editors)")
+        NSLog("\(favoriteRecipes)")
         ref.child("recipes").observeSingleEvent(of: .value, with: { (snapshot) in
-            
             for child in snapshot.children.allObjects as! [FIRDataSnapshot] {
                 let data = child.value as! [String:Any]
                 let recipe = Recipe()
@@ -39,17 +41,28 @@ class YummyData: NSObject {
                     self.editors.append(recipe)
                 }
             }
-            //self.save()
-            NSLog("\(self.recipes)")
+            self.save()
+//            NSLog("\(self.recipes)")
         })
     }
     
     func save(){
-        (self.editors as NSArray).write(toFile: YummyData.filePath, atomically:true)
-        (self.favoriteRecipes as NSArray).write(toFile: YummyData.filePath, atomically:true)
+        if(editors.count > 0){
+            NSKeyedArchiver.archiveRootObject(editors, toFile: YummyData.editorsSave)
+        }
+        if(favoriteRecipes.count > 0){
+            NSKeyedArchiver.archiveRootObject(favoriteRecipes, toFile: YummyData.favSave)
+        }
+//        (self.editors as NSArray).write(toFile: YummyData.editorsSave, atomically:true)
+//        (self.favoriteRecipes as NSArray).write(toFile: YummyData.favSave, atomically:true)
     }
     
     func loadOffline(){
-        editors = NSArray(contentsOfFile:YummyData.filePath) as! [Recipe]
+        if let loadArr: [Recipe] = NSKeyedUnarchiver.unarchiveObject(withFile: YummyData.editorsSave) as? [Recipe] {
+            editors = loadArr
+        }
+        if let loadArr: [Recipe] = NSKeyedUnarchiver.unarchiveObject(withFile: YummyData.favSave) as? [Recipe] {
+            favoriteRecipes = loadArr
+        }
     }
 }
