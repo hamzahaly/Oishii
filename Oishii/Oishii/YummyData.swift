af66115e0947885fcbaad1042773da5b3726a9c5
Thomas <tgents@uw.edu>
2016-12-08 19:04:44 -0800

    working on local storage

--- Oishii/Oishii/YummyData.swift
+++ Oishii/Oishii/YummyData.swift
@@ -12,7 +12,7 @@ import FirebaseDatabase
 
 class YummyData: NSObject {
     static let shared = YummyData()
-    static let path = Bundle.main.path(forResource: "data", ofType: "txt")!
+    private static let filePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "questions"
     var ref: FIRDatabaseReference!
     var recipes: [Recipe] = [Recipe]()
     var favoriteRecipes: [Recipe] = [Recipe]()
@@ -21,14 +21,15 @@ class YummyData: NSObject {
     func setup(){
         FIRApp.configure()
         ref = FIRDatabase.database().reference()
+        //loadOffline()
         ref.child("recipes").observeSingleEvent(of: .value, with: { (snapshot) in
+            
             for child in snapshot.children.allObjects as! [FIRDataSnapshot] {
                 let data = child.value as! [String:Any]
                 let recipe = Recipe()
                 recipe.name = data["name"] as! String
                 recipe.recipeid = data["recipeid"] as! String
-//                recipe.shortDescription = data["short_description"] as! String
-//                recipe.longDescription = data["long_description"] as! String
+                recipe.shortDescription = data["short_description"] as! String
                 recipe.ingredients = data["ingredients"] as! [String]
                 recipe.procedure = data["procedure"] as! [String]
                 recipe.categories = data["categories"] as! [String]
@@ -38,7 +39,17 @@ class YummyData: NSObject {
                     self.editors.append(recipe)
                 }
             }
-            print("\(self.recipes)")
+            self.save()
+            NSLog("\(self.recipes)")
         })
+    }
+    
+    func save(){
+        (self.editors as NSArray).write(toFile: YummyData.filePath, atomically:true)
+        (self.favoriteRecipes as NSArray).write(toFile: YummyData.filePath, atomically:true)
+    }
+    
+    func loadOffline(){
+        editors = NSArray(contentsOfFile:YummyData.filePath) as! [Recipe]
     }
 }
