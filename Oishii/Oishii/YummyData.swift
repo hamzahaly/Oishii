8a628d340c0bd9ccc9d89724c34388e696fd41c3
Thomas <tgents@gmail.com>
2016-12-09 16:58:38 -0800

    added firebase storage ref to yummydata, added some comments

--- Oishii/Oishii/YummyData.swift
+++ Oishii/Oishii/YummyData.swift
@@ -9,22 +9,33 @@
 import Foundation
 import Firebase
 import FirebaseDatabase
+import FirebaseStorage
 
 class YummyData: NSObject {
     static let shared = YummyData()
     private static let editorsSave = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "editors"
     private static let favSave = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "favorites"
     var ref: FIRDatabaseReference!
+    var storageRef: FIRStorageReference!
     var recipes: [Recipe] = [Recipe]()
     var favoriteRecipes: [Recipe] = [Recipe]()
     var editors: [Recipe] = [Recipe]()
     
     func setup(){
+        // configure Firebase
         FIRApp.configure()
+        
+        // get Firebase references
         ref = FIRDatabase.database().reference()
+        let storage = FIRStorage.storage()
+        storageRef = storage.reference(forURL: "gs://oishii-5ce2e.appspot.com")
+        
+        // start loading offline data
         loadOffline()
         NSLog("\(editors)")
         NSLog("\(favoriteRecipes)")
+        
+        // get online data
         ref.child("recipes").observeSingleEvent(of: .value, with: { (snapshot) in
             for child in snapshot.children.allObjects as! [FIRDataSnapshot] {
                 let data = child.value as! [String:Any]
@@ -42,8 +53,9 @@ class YummyData: NSObject {
                     self.editors.append(recipe)
                 }
             }
+            
+            // save to offline
             self.save()
-//            NSLog("\(self.recipes)")
         })
     }
     
@@ -54,8 +66,6 @@ class YummyData: NSObject {
         if(favoriteRecipes.count > 0){
             NSKeyedArchiver.archiveRootObject(favoriteRecipes, toFile: YummyData.favSave)
         }
-//        (self.editors as NSArray).write(toFile: YummyData.editorsSave, atomically:true)
-//        (self.favoriteRecipes as NSArray).write(toFile: YummyData.favSave, atomically:true)
     }
     
     func loadOffline(){
