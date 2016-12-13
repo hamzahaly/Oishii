63910094e85bec3e534ba7dc4cc1e86c130f35de
hamzahaly <hzhaly93@gmail.com>
2016-12-12 19:21:34 -0800

    changed green

--- Oishii/Oishii/AppDelegate.swift
+++ Oishii/Oishii/AppDelegate.swift
@@ -18,14 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         YummyData.shared.setup()
         // Override point for customization after application launch.
-        UINavigationBar.appearance().barTintColor = UIColor(red: 139/255, green: 204/225, blue: 131/255, alpha: 1.0)
+        UINavigationBar.appearance().barTintColor = UIColor(red: 139/255, green: 191/225, blue: 131/255, alpha: 1.0)
         
         
         UITabBar.appearance().tintColor = UIColor(red: 255/255, green: 133/225, blue: 152/255, alpha: 1.0)
-        UITabBar.appearance().barTintColor = UIColor(red: 139/255, green: 204/225, blue: 131/255, alpha: 1.0)
+        UITabBar.appearance().barTintColor = UIColor(red: 139/255, green: 191/225, blue: 131/255, alpha: 1.0)
         UITabBar.appearance().unselectedItemTintColor = UIColor.darkGray
         
-        UISearchBar.appearance().barTintColor = UIColor(red: 139/255, green: 204/225, blue: 131/255, alpha: 1.0)
+        UISearchBar.appearance().barTintColor = UIColor(red: 139/255, green: 191/225, blue: 131/255, alpha: 1.0)
         
         let cancelButtonAttributes: NSDictionary = [NSForegroundColorAttributeName: UIColor.darkGray]
         UIBarButtonItem.appearance().setTitleTextAttributes(cancelButtonAttributes as? [String: AnyObject], for: UIControlState.normal)
