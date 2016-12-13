05967045055f8782acd9aa30512d8d59385e8636
luludust <elizabethduong96@gmail.com>
2016-12-12 22:42:51 -0800

    Fix settings layout

--- Oishii/Oishii/SettingsTableViewController.swift
+++ Oishii/Oishii/SettingsTableViewController.swift
@@ -48,6 +48,10 @@ class SettingsTableViewController: UITableViewController {
         }
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
